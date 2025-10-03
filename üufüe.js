4540f09d85c979cb3d761857f96b639ef0d9b24b
SabirIsayev120 <sabirisayev120@gmail.com>
2025-10-03 20:18:53 +0400

    Update üufüe.js

--- üufüe.js
+++ üufüe.js
@@ -1,4 +1,11 @@
-let  adlar = [ "əli", "aysel","murad","sabir","Zəhra"]
-adlar[2]= "Classifed";
-console.log(adlar);
+let arr = ["New Hope", "Empire strikes back", "Return of the Jedi"];
 
+
+for (let i = 0; i < arr.length; i++) {
+
+    let dəfə = i + 4;
+
+    let message = `//Episode ${dəfə}: ${arr[i]}`;
+
+    console.log(message);
+}
