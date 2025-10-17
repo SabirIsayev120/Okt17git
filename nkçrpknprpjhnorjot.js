a2cfa6b7930f718c4fb36160bf233af9d7078c09
SabirIsayev120 <sabirisayev120@gmail.com>
2025-10-17 20:19:50 +0400

    Create nkçrpknprpjhnorjot.js

---
+++ nkçrpknprpjhnorjot.js
@@ -0,0 +1,17 @@
+const student = {
+  name: "Elmir",
+  age: 14,
+  grade: 85
+};
+
+console.log(student);
+
+
+student.grade = 90;
+
+console.log(student.grade);
+
+
+student.school = "Bakı məktəbi";
+
+console.log(student);
