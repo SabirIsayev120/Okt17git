8955931dd5d8d7965b1de4aa9a2ba0f7170804d0
SabirIsayev120 <sabirisayev120@gmail.com>
2025-10-28 20:10:40 +0400

    Update üufüe.js

--- developia/üufüe.js
+++ developia/üufüe.js
@@ -1,35 +1,17 @@
-let student = {
-  firstName: "Afsana",
-  lastName: "Talibova",
+function removeDuplicates(arr) {
+  return [...new Set(arr)];
+}
+
+const numbers = [1, 2, 2, 3, 4, 4, 4, 5];
+console.log(removeDuplicates(numbers));
+
+
+
+function countWords(words) {
+  const map = new Map();
+
   
-  get fullName() {
-    return this.firstName + " " + this.lastName;
-  },
-
-  set fullName(value) {
-    let parts = value.split(" "); 
-    this.firstName = parts[0];
-    this.lastName = parts[1];
-  }
-};
-
-student.fullName = "Lale Huseynova";
-console.log(student.fullName); 
-console.log(student.firstName);
-console.log(student.lastName); 
 
 
 
-
-
-let product = {
-  name: "Book",
-  priceUSD: 10,
-
-  get priceAZN() {
-    return this.priceUSD * 1.7;
-  }
-};
-
-console.log(product.name + " qiyməti: " + product.priceAZN + " AZN");
 
